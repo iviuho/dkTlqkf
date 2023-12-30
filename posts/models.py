4a6af2a834aa1fc7918d0bd904c22fd63537b4a7
iviuho <iviuho@gmail.com>
2023-12-30 19:48:09 +0900

    ✨ Add models(user, post)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    created_at = models.DateTimeField()
+    login_id = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    deleted_at = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
