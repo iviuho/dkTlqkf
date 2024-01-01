321c84d1feda786a30fac08ab7872f36d06d2317
iviuho <iviuho@gmail.com>
2024-01-01 21:08:11 +0900

    ⚡ Modify models(user, post)

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,28 @@
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class User(models.Model):
-    created_at = models.DateTimeField()
-    login_id = models.CharField(max_length=30)
-    password = models.CharField(max_length=20)
+    username_validator = UnicodeUsernameValidator()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    login_id = models.CharField(
+        max_length=30, unique=True, validators=[username_validator]
+    )
+    password = models.CharField(max_length=100)
 
 
 class Post(models.Model):
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.RESTRICT)
     content = models.CharField(max_length=500)
+    view_count = models.PositiveIntegerField(blank=True, default=0)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.RESTRICT,
+        related_name="comments",
+        null=True,
+        blank=True,
+    )
