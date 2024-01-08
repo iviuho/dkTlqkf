1382680db76c933d599864783036073643c86e41
iviuho <iviuho@gmail.com>
2024-01-08 14:39:47 +0900

    ✨ Add home app

--- dkTlqkf/settings.py
+++ dkTlqkf/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home.apps.HomeConfig",
     "posts.apps.PostsConfig",
     "users.apps.UsersConfig",
 ]
@@ -91,6 +92,9 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
