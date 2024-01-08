02d23bef6a4d71ad87be55852bbf871379b5f573
iviuho <iviuho@gmail.com>
2024-01-08 15:04:39 +0900

    🔥 Remove user model

--- users/models.py
+++ users/models.py
@@ -1,9 +1,3 @@
-from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-
-class User(AbstractBaseUser):
-    created_at = models.DateTimeField(auto_now_add=True)
-    username = models.CharField(max_length=30, unique=True)
-
-    USERNAME_FIELD = "username"
+# Create your models here.
